a85d3e088910f984ad31156659b783c0ac54951e
Josue-H <55160786+Josue-H@users.noreply.github.com>
2025-05-31 11:25:44 -0600

    Update llamada_openai_simulada.py

--- services/llamada_openai_simulada.py
+++ services/llamada_openai_simulada.py
@@ -20,7 +20,7 @@ async def llamar_a_modelo_simulado(image_url: str) -> dict:
                     {
                         "type": "text",
                         "text": (
-                            "Observa esta imagen de un cultivo y responde usando el siguiente formato, sin estilos de letras, solo en el formato, , si no es un cultivo natural solo pon en el nombre "No es un cultivo":\n\n"
+                            "Observa esta imagen de un cultivo y responde usando el siguiente formato, sin estilos de letras, solo en el formato, , si no es un cultivo natural solo pon en el nombre "No es un cultivo", en el nombre no pongas otros comentarios, solo el nombre más certero:\n\n"
                             "nombre\n"
                             "tipo_de_cultivo:\n"
                             "edad_aproximada:\n"
