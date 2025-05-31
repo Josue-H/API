61b05f152d4c49ac8d9b501295772d1cd6ede9ff
Josue-H <josuecochoher@gmail.com>
2025-05-30 20:06:37 -0600

    commit añadiendo upload

--- services/imgur_uploader.py
+++ services/imgur_uploader.py
@@ -1,25 +1,39 @@
-import base64
+import os
 import httpx
-# from config import IMGUR_CLIENT_ID
+import base64
+from urllib.parse import urlencode
+import requests
+
+UPLOADCARE_PUBLIC_KEY = "34d20300d53b98b547bb"
+
+
 
 async def upload_image_to_imgur(base64_str: str) -> str:
-    if "," not in base64_str:
-        raise ValueError("Formato base64 inválido. Falta encabezado.")
+    if "," not in base64_str or not base64_str.startswith("data:image"):
+        raise ValueError("⚠️ Formato base64 inválido. Falta encabezado tipo data:image/...")
 
-    b64_data = base64_str.split(",")[1]
+    header, b64_data = base64_str.split(",", 1)
+    mime_type = header.split(";")[0].split(":")[1]
+
     image_bytes = base64.b64decode(b64_data)
 
-    headers = {"Authorization": "Client-ID 2651b0967ee70c4"}
+    files = {
+        'file': ('image.jpg', image_bytes, mime_type)
+    }
 
-
-    # 🔁 Usamos files, igual que en requests
-    files = {"image": ("image.jpg", image_bytes, "image/jpeg")}
+    data = {
+        'UPLOADCARE_PUB_KEY': UPLOADCARE_PUBLIC_KEY,
+        'UPLOADCARE_STORE': '1'
+    }
 
     async with httpx.AsyncClient() as client:
-        response = await client.post("https://api.imgur.com/3/image", headers=headers, files=files)
+        response = await client.post('https://upload.uploadcare.com/base/', data=data, files=files)
 
     if response.status_code != 200:
-        print("🛑 Imgur error:", response.status_code, response.text)
-        raise Exception("Error al subir imagen a Imgur")
+        raise Exception(f"Error al subir imagen a Uploadcare: {response.status_code} {response.text}")
 
-    return response.json()["data"]["link"]
+    file_id = response.json().get("file")
+    if not file_id:
+        raise Exception("❌ No se encontró el campo 'file' en la respuesta.")
+
+    return f"https://ucarecdn.com/{file_id}/"
